c7523bb5368bb06af2d73e162ccb785d600826b0
rakha <babanrakha@gmail.com.com>
2024-05-27 12:28:13 +0700

    new

--- routes/web.php
+++ routes/web.php
@@ -2,50 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('beranda');
-});
+Route::view('/', 'welcome');
 
-Route::get('/customer', function () {
-    return view('customer');
-});
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-});
+Route::view('profile', 'profile')
+    ->middleware(['auth'])
+    ->name('profile');
 
-Route::get('/pesanan', function () {
-    return view('pesanan');
-});
-
-Route::get('/create/customer', function () {
-    return view('create_customer');
-});
-
-Route::get('/create/pesanan', function () {
-    return view('create_pesanan');
-});
-
-Route::get('/barangpesanan', function () {
-    return view('barang_pesanan');
-});
-
-Route::get('/edit/barangpesanan', function () {
-    return view('edit_barang_pesanan');
-});
-
-Route::get('/create/barangpesanan', function () {
-    return view('create_barang_pesanan');
-});
-
-Route::get('/login', function () {
-    return view('login');
-});
-
-Route::get('/register', function () {
-    return view('register');
-});
-
-
-
-
+require __DIR__.'/auth.php';
